55902ca4ad5ffc6262b0c17c90ba70889c53f87c
OussamaEcommerce <OussamaEcommerce@gmail.com>
2022-02-12 15:44:18 +0100

    responsive

--- webpack.config.js
+++ webpack.config.js
@@ -98,6 +98,11 @@ module.exports = {
     filename: "index.html", 
     template: "./src/index.html",
   }),
+
+  new HtmlWebpackPlugin({
+    filename: "product.html", 
+    template: "./src/product.html",
+  }),
   new MiniCssExtractPlugin({filename: "css/style.css"}),
 
   new OptimizeCssAssetsPlugin({}),
